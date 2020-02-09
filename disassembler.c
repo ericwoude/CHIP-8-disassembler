3c7bc3b51e686dc2e50c9b7305fe36b5817bb372
Eric van der Woude <ericvdwoude@gmail.com>
2020-02-09 23:23:53 +0100

    Added comments, renamed variables, made code style uniform

--- disassembler.c
+++ disassembler.c
@@ -2,20 +2,30 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define MEM_OFFSET 0x200
+#define MEM_OFFSET 0x200 // Programs are always loaded at 0x200.
 
-void disassembleCHIP8(uint8_t *buffer, int pc)
+void disassembleCHIP8(uint8_t* buffer, int pc)
 {
-    uint8_t *code = &buffer[pc];
-    uint8_t nibble = code[0] >> 4;
+    uint8_t* op = &buffer[pc];
+    uint8_t nibble = op[0] >> 4;
 
-    printf("%04X %02X %02X  ", pc, code[0], code[1]);
+    printf("%04X %02X %02X  ", pc, op[0], op[1]);
+
+    /*
+     * The CHIP-8 has 35 different opops of 2 bytes in length.
+     * These bytes are stored in op[0] and op[1] respectively.
+     *
+     * For most opcodes, the instruction can be deduced from the
+     * 4 left-most bits, while the remaining 12 bits are used for
+     * constants or register identifiers. For specifics, refer
+     * to the Wikipedia page on CHIP-8.
+     */
 
     switch(nibble)
     {
         case 0x0:
         {
-            switch(code[1])
+            switch(op[1])
             {
                 case 0xe0:
                     printf("%-10s\n", "CLS");
@@ -29,57 +39,57 @@ void disassembleCHIP8(uint8_t *buffer, int pc)
             break;
         }
         case 0x01:
-            printf("%-10s $%01X%02X\n", "JUMP", code[0] & 0xf, code[1]);
+            printf("%-10s $%01X%02X\n", "JUMP", op[0] & 0xf, op[1]);
             break;
         case 0x02:
-            printf("%-10s $%01X%02X\n", "CALL", code[0] & 0xf, code[1]);
+            printf("%-10s $%01X%02X\n", "CALL", op[0] & 0xf, op[1]);
             break;
         case 0x03:
-            printf("%-10s V%01X, #$%02X\n", "SKIP.EQ", code[0] & 0xf, code[1]);
+            printf("%-10s V%01X, #$%02X\n", "SKIP.EQ", op[0] & 0xf, op[1]);
             break;
         case 0x04:
-            printf("%-10s V%01X, #$%02X\n", "SKIP.NE", code[0] & 0xf, code[1]);
+            printf("%-10s V%01X, #$%02X\n", "SKIP.NE", op[0] & 0xf, op[1]);
             break;
         case 0x05:
-            printf("%-10s V%01X, V%01X\n", "SKIP.EQ", code[0] & 0xf, code[1]);
+            printf("%-10s V%01X, V%01X\n", "SKIP.EQ", op[0] & 0xf, op[1]);
             break;
         case 0x06:
-            printf("%-10s V%01X, #$%02X\n", "MVI", code[0] & 0x0f, code[1]);
+            printf("%-10s V%01X, #$%02X\n", "MVI", op[0] & 0x0f, op[1]);
             break;
         case 0x07:
-            printf("%-10s V%01X, #$%02X\n", "ADI", code[0] & 0x0f, code[1]);
+            printf("%-10s V%01X, #$%02X\n", "ADI", op[0] & 0x0f, op[1]);
             break;
         case 0x08:
         {
-            uint8_t operation = code[1] & 0x0f;
+            uint8_t operation = op[1] & 0x0f;
             switch (operation)
             {
                 case 0x0:
-                    printf("%-10s V%01X, V%01X\n", "MOV", code[0] & 0x0f, code[1] >> 4);
+                    printf("%-10s V%01X, V%01X\n", "MOV", op[0] & 0x0f, op[1] >> 4);
                     break;
                 case 0x1:
-                    printf("%-10s V%01X, V%01X\n", "OR", code[0] & 0x0f, code[1] >> 4);              
+                    printf("%-10s V%01X, V%01X\n", "OR", op[0] & 0x0f, op[1] >> 4);
                     break;
                 case 0x2:
-                    printf("%-10s V%01X, V%01X\n", "AND", code[0] & 0x0f, code[1] >> 4);              
+                    printf("%-10s V%01X, V%01X\n", "AND", op[0] & 0x0f, op[1] >> 4);
                     break;
                 case 0x3:
-                    printf("%-10s V%01X, V%01X\n", "XOR", code[0] & 0x0f, code[1] >> 4);              
+                    printf("%-10s V%01X, V%01X\n", "XOR", op[0] & 0x0f, op[1] >> 4);
                     break;
                 case 0x4:
-                    printf("%-10s V%01X, V%01X\n", "ADD", code[0] & 0x0f, code[1] >> 4);              
+                    printf("%-10s V%01X, V%01X\n", "ADD", op[0] & 0x0f, op[1] >> 4);
                     break;
                 case 0x5:
-                    printf("%-10s V%01X, V%01X\n", "SUB", code[0] & 0x0f, code[1] >> 4);              
-                    break;                    
+                    printf("%-10s V%01X, V%01X\n", "SUB", op[0] & 0x0f, op[1] >> 4);
+                    break;
                 case 0x6:
-                    printf("%-10s V%01X, V%01X\n", "SHR", code[0] & 0x0f, code[1] >> 4);              
-                    break;                    
+                    printf("%-10s V%01X, V%01X\n", "SHR", op[0] & 0x0f, op[1] >> 4);
+                    break;
                 case 0x7:
-                    printf("%-10s V%01X, V%01X, V%01X\n", "SUB", code[0] & 0x0f, code[1] >> 4, code[0] & 0x0f);              
+                    printf("%-10s V%01X, V%01X, V%01X\n", "SUB", op[0] & 0x0f, op[1] >> 4, op[0] & 0x0f);
                     break;
                 case 0xe:
-                    printf("%-10s V%01X, V%01X\n", "SHL", code[0] & 0x0f, code[1] >> 4);
+                    printf("%-10s V%01X, V%01X\n", "SHL", op[0] & 0x0f, op[1] >> 4);
                     break;
                 default:
                     printf("%-10s\n", "UNKNOWN 8");
@@ -87,30 +97,30 @@ void disassembleCHIP8(uint8_t *buffer, int pc)
             break;
         }
         case 0x9:
-            printf("%-10s V%01X, V%01X\n", "SKIP.NE", code[0] & 0x0f, code[1] >> 4);
+            printf("%-10s V%01X, V%01X\n", "SKIP.NE", op[0] & 0x0f, op[1] >> 4);
             break;
         case 0xa:
-            printf("%-10s I,  #$%01X%02X\n", "MVI", code[0] & 0xf, code[1]);
+            printf("%-10s I,  #$%01X%02X\n", "MVI", op[0] & 0xf, op[1]);
             break;
         case 0xb:
-            printf("%-10s $%01X%02X(V0)\n", "JUMP", code[0] & 0xf, code[1]);
+            printf("%-10s $%01X%02X(V0)\n", "JUMP", op[0] & 0xf, op[1]);
             break;
         case 0xc:
-            printf("%-10s V%01X, #$%02X\n", "RNDMSK", code[0] & 0xf, code[1]);
+            printf("%-10s V%01X, #$%02X\n", "RNDMSK", op[0] & 0xf, op[1]);
             break;
         case 0xd:
-            printf("%-10s V%01X, V%01X, #$%01X\n", "RNDMSK", code[0] & 0xf, code[1] >> 4, code[1] & 0x0f);   
-            break;         
+            printf("%-10s V%01X, V%01X, #$%01X\n", "RNDMSK", op[0] & 0xf, op[1] >> 4, op[1] & 0x0f);
+            break;
         case 0xe:
         {
-            uint8_t instruction = code[1] & 0x0f;
+            uint8_t instruction = op[1] & 0x0f;
             switch(instruction)
             {
                 case 0x9e:
-                    printf("%-10s V%01X\n", "SKIPKEY.Y", code[0] & 0xf);
+                    printf("%-10s V%01X\n", "SKIPKEY.Y", op[0] & 0xf);
                     break;
                 case 0xa1:
-                    printf("%-10s V%01X\n", "SKIPKEY.N", code[0] & 0xf);
+                    printf("%-10s V%01X\n", "SKIPKEY.N", op[0] & 0xf);
                     break;
                 default:
                     printf("%-10s\n", "UNKNOWN E");
@@ -119,35 +129,35 @@ void disassembleCHIP8(uint8_t *buffer, int pc)
         }
         case 0xf:
         {
-            uint8_t instruction = code[1];
+            uint8_t instruction = op[1];
             switch(instruction)
             {
 				case 0x07:
-                    printf("%-10s V%01X, DELAY\n", "MOV", code[0] & 0xf);
+                    printf("%-10s V%01X, DELAY\n", "MOV", op[0] & 0xf);
                     break;
 				case 0x1e:
-                    printf("%-10s I, V%01X\n", "ADI", code[0] & 0xf);
+                    printf("%-10s I, V%01X\n", "ADI", op[0] & 0xf);
                     break;
 				case 0x0a:
-                    printf("%-10s V%01X\n", "KEY", code[0] & 0xf);
+                    printf("%-10s V%01X\n", "KEY", op[0] & 0xf);
                     break;
 				case 0x15:
-                    printf("%-10s DELAY, V%01X\n", "MOV", code[0] & 0xf);
+                    printf("%-10s DELAY, V%01X\n", "MOV", op[0] & 0xf);
                     break;
 				case 0x18:
-                    printf("%-10s SOUND, V%01X\n", "MOV", code[0] & 0xf);
+                    printf("%-10s SOUND, V%01X\n", "MOV", op[0] & 0xf);
                     break;
 				case 0x29:
-                    printf("%-10s I, V%01X\n", "SPRITECHAR", code[0] & 0xf);
+                    printf("%-10s I, V%01X\n", "SPRITECHAR", op[0] & 0xf);
                     break;
 				case 0x33:
-                    printf("%-10s (I), V%01X\n", "MOVBCD", code[0] & 0xf);
+                    printf("%-10s (I), V%01X\n", "MOVBCD", op[0] & 0xf);
                     break;
 				case 0x55:
-                    printf("%-10s (I), V0-V%01X\n", "MOVM", code[0] & 0xf);
+                    printf("%-10s (I), V0-V%01X\n", "MOVM", op[0] & 0xf);
                     break;
 				case 0x65:
-                    printf("%-10s V0-V%01X, (I)\n", "MOVM", code[0] & 0xf);
+                    printf("%-10s V0-V%01X, (I)\n", "MOVM", op[0] & 0xf);
                     break;
                 default:
                     printf("%-10s\n", "UNKNOWN F");
@@ -159,36 +169,37 @@ void disassembleCHIP8(uint8_t *buffer, int pc)
     }
 }
 
-uint8_t *load_file(char *file_location, int *file_size)
+uint8_t* load_file(char* file_location, int* file_size)
 {
-    FILE *fp = fopen(file_location, "rb");
+    FILE* fp = fopen(file_location, "rb");
     if (fp == NULL)
     {
         printf("Error opening file: %s\n", file_location);
         exit(EXIT_FAILURE);
     }
 
-    // Get the file size, allocate memory and read the data into allocated memory.
+    // Get the file size and read data into allocated memory.
     fseek(fp, 0L, SEEK_END);
     *file_size = ftell(fp);
     rewind(fp);
-    uint8_t *buffer = malloc(*file_size + MEM_OFFSET);
+
+    uint8_t* buffer = malloc(*file_size + MEM_OFFSET);
     fread(buffer + MEM_OFFSET, *file_size, 1, fp);
     fclose(fp);
 
     return buffer;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     if (argv[1] == NULL)
     {
-        printf("Usage: ./disassembleCHIP8 file");
-        exit(EXIT_FAILURE); 
+        printf("Usage: ./disassembleCHIP8 file\n");
+        exit(EXIT_FAILURE);
     }
 
     int file_size;
-    uint8_t *buffer = load_file(argv[1], &file_size);
+    uint8_t* buffer = load_file(argv[1], &file_size);
 
     for (int pc = MEM_OFFSET; pc < file_size + MEM_OFFSET; pc += 2)
     {
